68c8296da39395b440a758cc2e79eada87926550
Junjun <djoenaedi12@gmail.com>
2023-03-27 08:58:13 +0700

    first commit

---
+++ src/main/java/com/gasi/springbootgcploggingexample/SpringBootGcpLoggingExampleApplication.java
@@ -0,0 +1,13 @@
+package com.gasi.springbootgcploggingexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGcpLoggingExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGcpLoggingExampleApplication.class, args);
+	}
+
+}
