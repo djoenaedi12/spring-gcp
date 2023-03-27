ebc2b7074ccceaa2ed8038d7dea79cc0eff04f35
Junjun <djoenaedi12@gmail.com>
2023-03-27 09:52:58 +0700

    first commit 3

---
+++ src/test/java/com/gasi/springbootgcploggingexample/SpringBootGcpLoggingExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gasi.springbootgcploggingexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGcpLoggingExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
